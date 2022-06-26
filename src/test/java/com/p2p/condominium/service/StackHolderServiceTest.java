d71b744be93968104a8aff6a74b1a300abc86803
Geraldo Francisco Conselho <geraldof.neto2016@gmail.com>
2022-06-26 20:09:29 -0300

    testado find by id

--- src/test/java/com/p2p/condominium/service/StackHolderServiceTest.java
+++ src/test/java/com/p2p/condominium/service/StackHolderServiceTest.java
@@ -4,7 +4,9 @@ import com.p2p.condominium.document.AddressDocument;
 import com.p2p.condominium.document.StackHolderDocument;
 import com.p2p.condominium.dto.Address;
 import com.p2p.condominium.dto.StackHolderInsertRequest;
+import com.p2p.condominium.dto.StackHolderUpdateRequest;
 import com.p2p.condominium.enums.TypePersonEnum;
+import com.p2p.condominium.exception.BusinessException;
 import com.p2p.condominium.repository.StackHolderRepository;
 import com.p2p.condominium.service.impl.StackHolderServiceImpl;
 import org.junit.Test;
@@ -43,6 +45,30 @@ public class StackHolderServiceTest {
                     assertNotNull(response);
                     assertNotNull(response.getId());
                 }).verifyComplete();
+    }
+
+    @Test
+    public void findByIdSuccessTest() {
+        when(repository.findById(anyString())).thenReturn(Mono.just(getDocumentPhysicalPersonReturn().build()));
+        final var result = this.service.findById(anyString());
+        StepVerifier.create(result).assertNext(response -> assertNotNull(response)).verifyComplete();
+    }
+
+    @Test
+    public void findByIdErrorTest() {
+        when(repository.findById(anyString())).thenReturn(Mono.empty());
+        final var result = this.service.findById(anyString());
+        StepVerifier.create(result).expectError(BusinessException.class).verify();
+    }
+
+    private StackHolderUpdateRequest.StackHolderUpdateRequestBuilder getUpdateRequest() {
+        return StackHolderUpdateRequest.builder()
+                .id(randomUUID().toString())
+                .cpf("98487828000")
+                .email("email@email.com")
+                .name("Gihyondîs")
+                .address(Address.builder().build())
+                .phones(new ArrayList<>());
     }
 
     private StackHolderInsertRequest.StackHolderInsertRequestBuilder getInsertRequest() {
