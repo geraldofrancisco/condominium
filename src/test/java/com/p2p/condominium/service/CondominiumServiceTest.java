fd64e96f6ac8cbc50371a122ef463015582b51ad
Geraldo Francisco Conselho <geraldof.neto2016@gmail.com>
2022-07-09 23:32:09 -0300

    testando findbyid e findall

--- src/test/java/com/p2p/condominium/service/CondominiumServiceTest.java
+++ src/test/java/com/p2p/condominium/service/CondominiumServiceTest.java
@@ -1,11 +1,19 @@
 package com.p2p.condominium.service;
 
+import com.p2p.condominium.document.AddressDocument;
+import com.p2p.condominium.document.CondominiumDocument;
+import com.p2p.condominium.document.StackHolderDocument;
+import com.p2p.condominium.dto.Address;
+import com.p2p.condominium.dto.CondominiumDTO;
+import com.p2p.condominium.exception.BusinessException;
 import com.p2p.condominium.mapper.AddressMapper;
 import com.p2p.condominium.mapper.CondominiumManagerMapper;
 import com.p2p.condominium.mapper.CondominiumMapper;
 import com.p2p.condominium.mapper.PaginatedResponseMapper;
 import com.p2p.condominium.repository.CondominiumRepository;
 import com.p2p.condominium.repository.StackHolderRepository;
+import com.p2p.condominium.service.impl.CondominiumServiceImpl;
+import com.p2p.condominium.service.impl.StackHolderServiceImpl;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -13,11 +21,21 @@ import org.mapstruct.factory.Mappers;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.util.ReflectionTestUtils;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
 
+import java.util.ArrayList;
+import java.util.UUID;
+
+import static com.p2p.condominium.enums.TypePersonEnum.FISICA;
+import static com.p2p.condominium.enums.TypePersonEnum.JURIDICA;
+import static java.util.UUID.randomUUID;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
 
@@ -26,7 +44,10 @@ import static org.mockito.Mockito.when;
 public class CondominiumServiceTest {
 
     @InjectMocks
-    private CondominiumService service;
+    private CondominiumServiceImpl service;
+
+    @MockBean
+    private StackHolderServiceImpl stackHolderService;
 
     @Mock
     private CondominiumRepository repository;
@@ -47,9 +68,61 @@ public class CondominiumServiceTest {
     }
 
     @Test
-    public void insertSuccessTest() {
-        when(repository.findByIdentification(anyString())).thenReturn(Mono.empty());
+    public void findByIdSuccessTest() {
+        var id = UUID.randomUUID().toString();
+        when(repository.findById(id)).thenReturn(Mono.just(getCondominiumDocument().build()));
+        var result = this.service.findById(id);
+        StepVerifier.create(result).assertNext(response -> assertNotNull(response)).verifyComplete();
     }
+
+    @Test
+    public void findByIdErrorTest() {
+        var id = UUID.randomUUID().toString();
+        when(repository.findById(id)).thenReturn(Mono.empty());
+        var result = this.service.findById(id);
+        StepVerifier.create(result).expectError(BusinessException.class).verify();
+    }
+
+    @Test
+    public void findAllSuccessTest() {
+        var pageable = PageRequest.of(0, 1);
+        when(repository.count()).thenReturn(Mono.just(1L));
+        when(repository.findByIdNotNullOrderByNameAsc(pageable)).thenReturn(Flux.just(getCondominiumDocument().build()));
+
+        final var result = this.service.findAll(pageable);
+        StepVerifier.create(result).assertNext(response -> assertNotNull(response)).verifyComplete();
+    }
+
+
+
+    private CondominiumDocument.CondominiumDocumentBuilder getCondominiumDocument() {
+        return CondominiumDocument.builder()
+
+                .address(AddressDocument.builder().build())
+                .name("Parque Serra dos Ipês")
+                .constructionCompany(UUID.randomUUID().toString())
+                .identification("79765474000184");
+    }
+
+    private CondominiumDTO.CondominiumDTOBuilder getCondominiumDTO() {
+        return CondominiumDTO.builder()
+                .address(Address.builder().build())
+                .name("Parque Serra dos Ipês")
+                .constructionCompanyId(UUID.randomUUID().toString())
+                .identification("79765474000184");
+    }
+
+    private StackHolderDocument.StackHolderDocumentBuilder getStackHolderDocumentReturn() {
+        return StackHolderDocument.builder()
+                .id(randomUUID().toString())
+                .name("Gihyondîs")
+                .address(AddressDocument.builder().build())
+                .email("email@email.com")
+                .identification("98487828000")
+                .typePersonEnum(FISICA)
+                .phones(new ArrayList<>());
+    }
+
 
 
 }
