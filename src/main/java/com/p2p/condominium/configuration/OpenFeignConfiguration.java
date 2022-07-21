07a34c805195657b15bd902cce7175d1d74efcec
Geraldo Francisco Conselho <geraldof.neto2016@gmail.com>
2022-07-21 12:02:11 -0300

    adicionado openfeign e eureka client

---
+++ src/main/java/com/p2p/condominium/configuration/OpenFeignConfiguration.java
@@ -0,0 +1,15 @@
+package com.p2p.condominium.configuration;
+
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
+public class OpenFeignConfiguration {
+    @Bean
+    @LoadBalanced
+    public WebClient.Builder loadBalancedWebClientBuilder() {
+        return WebClient.builder();
+    }
+}
